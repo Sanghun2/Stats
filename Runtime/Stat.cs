006607f1c54919004c5b5063b4a1e3af0d19d7c1
lshzang86 <lshzang86@naver.com>
2026-02-13 16:15:32 +0900

    Stat Git Init

---
+++ Runtime/Stat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace BilliotGames
+{
+    public class Stat : ValueContainer
+    {
+        public string ID => id;
+
+        [SerializeField] string id;
+
+        public Stat(string id) {
+            this.id = id;
+        }
+
+        public Stat(string id, float maxValue) {
+            this.id = id;
+            this.maxValue = maxValue;            
+        }
+    }
+}
