90b3eb483265ba641c9b9715d51e17a540efaed8
lshzang86 <lshzang86@naver.com>
2026-02-28 15:28:36 +0900

    Stat Modifier 기능 추가

---
+++ Runtime/Class/Modifier/StatModifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace BilliotGames
+{
+    [Serializable]
+    public class StatModifier : IEquatable<StatModifier>
+    {
+        public enum ModifierType {
+            PureAdd,
+            PureMultiply,
+            PercentAdd,
+        }
+
+        public string ModifierID => modifierID;
+
+        private string modifierID;
+        private float modifierValue;
+        private ModifierType modifierType;
+
+        public float ApplyValue(float targetValue) {
+            return ModifyValue(targetValue, modifierValue, modifierType);
+        }
+
+        private float ModifyValue(float targetValue, float modifierValue,  ModifierType modifierType) {
+            switch (modifierType) {
+                case ModifierType.PureAdd:
+                    return targetValue + modifierValue;
+                case ModifierType.PureMultiply:
+                    return targetValue * modifierValue;
+                case ModifierType.PercentAdd:
+                    return targetValue * (1 + modifierValue);
+                default:
+                    Debug.LogError($"<color=red>No mod type exist</color>");
+                    return targetValue;
+            }
+        }
+
+        public bool Equals(StatModifier other) {
+            if (other == null) return false;
+            if (!modifierID.Equals(other.modifierID)) return false;
+            if (!Mathf.Approximately(modifierValue, other.modifierValue)) return false;
+            if (modifierType != other.modifierType) return false;
+            return true;
+        }
+    }
+}
--- Runtime/Class/Stat/Stat.cs
+++ Runtime/Class/Stat/Stat.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BilliotGames
@@ -7,9 +8,25 @@ namespace BilliotGames
     public class Stat
     {
         public virtual Value<float> Value => new Value<float>(value, deltaValue:0);
+        public virtual float FinalValue
+        {
+            get
+            {
+                if (modifierDirty) {
+                    cachedFinalValue = CalculateFinalValue(value);
+                    modifierDirty = false;
+                }
+
+                return cachedFinalValue;
+            }
+        }
 
         [SerializeField] protected string id;
         [SerializeField] protected float value;
+
+        private List<StatModifier> modifierList = new();
+        private bool modifierDirty = true;
+        private float cachedFinalValue;
 
         public virtual event Action<Value<float>> OnValueChanged;
 
@@ -17,13 +34,44 @@ namespace BilliotGames
             this.id = id;
         }
 
-        public Stat(float value) {
-            this.value = value;
+        public Stat(float baseValue) {
+            this.value = baseValue;
         }
 
         public virtual void ChangeValue(float delataValue) {
             value += delataValue;
             OnValueChanged?.Invoke(new Value<float>(value, delataValue));
         }
+
+        #region Modifiers
+
+        public void AddModifier(StatModifier modifier) {
+            modifierList.Add(modifier);
+            modifierDirty = true;
+        }
+        public void RemoveModifier(StatModifier targetModifier) {
+            if (targetModifier == null) return;
+
+            for (int i = modifierList.Count-1; i >= 0 ; --i) {
+                var modifier = modifierList[i];
+                if (modifier.Equals(targetModifier)) {
+                    modifierList.RemoveAt(i);
+                }
+            }
+
+            modifierDirty = true;
+        }
+
+
+        private float CalculateFinalValue(float value) {
+            for (int i = 0; i < modifierList.Count; ++i) {
+                var modifier = modifierList[i];
+                value = modifier.ApplyValue(value);
+            }
+
+            return value;
+        }
+
+        #endregion
     }
 }
